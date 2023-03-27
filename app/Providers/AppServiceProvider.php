465db682bb12f06611697cce29ddd952b04fbf7e
Eduard Boikov <eduard.boikov@edu2x.com>
2023-03-27 16:57:58 +0300

    Added AnswerRepositoryContract binding

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,11 @@
 
 namespace App\Providers;
 
+use App\Contracts\AnswerRepositoryContract;
 use Illuminate\Support\ServiceProvider;
 use App\Contracts\QuestionRepositoryContract;
 use App\Contracts\QuestionServiceContract;
+use App\Repositories\AnswerRepository;
 use App\Repositories\QuestionRepository;
 use App\Services\QuestionService;
 
@@ -17,6 +19,7 @@ class AppServiceProvider extends ServiceProvider
     {
         $this->app->bind(QuestionServiceContract::class, QuestionService::class);
         $this->app->bind(QuestionRepositoryContract::class, QuestionRepository::class);
+        $this->app->bind(AnswerRepositoryContract::class, AnswerRepository::class);
     }
 
     /**
